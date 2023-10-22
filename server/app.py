3f39582ad66fd6701cef88bc9d200a6f5d781a88
Elliot Maisl <elliot.maisl@gmail.com>
2023-10-22 19:44:13 +0200

    Add flask server for the new classifier

--- server/app.py
+++ server/app.py
@@ -1,23 +1,54 @@
 from flask import Flask, request
 from PIL import Image
-from transformers import pipeline
+import torch
+from torchvision.transforms import v2 as transforms
+from models.classifier import MultiLabelImageClassifierModel, normalize_image, transform_image
 
 app = Flask(__name__)
-new_size = 224
-classifier = pipeline("image-classification", model="resnet-50-finetuned-eye-disease-2")
 
-def resize_pil_image(image):
+LABELS = ['Disease_Risk', 'DR', 'ARMD', 'MH', 'DN', 'MYA', 'BRVO', 'TSLN', 'LS', 'CSR', 'ODC', 'CRVO', 'ODP', 'ODE', 'RS', 'CRS', 'RPEC']
+IMAGE_SIZE = 250
+NUM_CHANNELS = 7
+TEST_SCORE_THRESHOLD = 0.1
+
+model = MultiLabelImageClassifierModel(num_classes=len(LABELS), input_size=IMAGE_SIZE, num_channels=NUM_CHANNELS)
+model.load_state_dict(torch.load(f'./models/Hope.pth'))
+model.eval()
+
+def prepare_pil_image(image):
     image = Image.open(image)
-    aspect_ratio = image.size[0] / image.size[1]
-    new_width = int(new_size * aspect_ratio)
-    left = (new_width - new_size) / 2
-    right = (new_width + new_size) / 2
 
-    return image.resize((new_width, new_size)) \
-                .crop((left, 0, right, new_size)) \
-                .resize((new_size, new_size))
+    image = transforms.Compose([
+        transforms.ToImage(),
+        transforms.ToDtype(torch.float32, scale=True),
+    ])(image)
+
+    image = normalize_image(image)
+
+    image = transforms.ToPILImage()(image)
+    image_layers = transform_image(image)
+
+    for i, img in enumerate(image_layers):
+        image_layers[i] = transforms.Compose([
+            transforms.ToImage(),
+            transforms.ToDtype(torch.float32, scale=True),
+        ])(img)
+
+    image = torch.cat(image_layers, dim=0)
+    image = image.unsqueeze(0)
+
+    return image
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    image = resize_pil_image(request.files['image'])
-    return {"predictions": classifier(image)}
+    image = prepare_pil_image(request.files['image'])
+
+    predictions = model(image)
+    predictions = sorted(enumerate(predictions[0]), key=lambda x: x[1], reverse=True)
+    predictions = [(LABELS[label_idx], score) for label_idx, score in predictions if score > TEST_SCORE_THRESHOLD]
+
+    return {
+        "predictions": [
+            {"label": label, "score": float(score)} for label, score in predictions
+        ]
+    }
